c1b3b68a4f14335083b3bef45ccdb0770709ab07
Ryan Poe <baublet@gmail.com>
2019-02-24 12:32:46 -0600

    Add useForm custom hook to use when creating new forms -- TODO: add this to various existing forms

---
+++ client/helpers/useForm.ts
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+type ValuesType = object;
+
+export interface UseFormType {
+  handleChange: (event: React.FormEvent<HTMLInputElement>) => void;
+  handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  values: ValuesType;
+}
+
+export type UseFormCallback = (values: ValuesType) => void;
+
+export default function useForm(
+  callback: UseFormCallback,
+  initialValues: ValuesType
+): UseFormType {
+  const [values, setValues] = useState(initialValues);
+
+  const handleSubmit = (event: React.FormEvent) => {
+    if (event) event.preventDefault();
+    callback(values);
+  };
+
+  const handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    event.persist();
+    setValues(values => ({
+      ...values,
+      [event.currentTarget.name]: event.currentTarget.value
+    }));
+  };
+
+  return {
+    handleChange,
+    handleSubmit,
+    values
+  };
+}

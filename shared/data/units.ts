e70634c8e9d847e9d4ded6cdd04512f9e1392f1a
Ryan Poe <baublet@gmail.com>
2019-04-19 10:36:02 -0400

    WIP update on home-grown units conversion

---
+++ shared/data/units.ts
@@ -0,0 +1,52 @@
+export const massUnits = {
+  g: 1,
+  kg: 1000,
+  kilo: 1000,
+  kilos: 1000,
+  kilogram: 1000,
+  kilograms: 1000,
+  lb: 453.592,
+  pounds: 453.592,
+  pound: 453.592,
+  st: 6350.29,
+  stone: 6350.29,
+  stones: 6350.29
+};
+
+export const distanceUnits = {
+  cm: 1,
+  centimeter: 1,
+  m: 100,
+  meter: 100,
+  meters: 100,
+  k: 100000,
+  km: 100000,
+  kilometer: 100000,
+  kilometers: 100000,
+  yd: 91.44,
+  yard: 91.44,
+  yards: 91.44,
+  mi: 160934,
+  mis: 160934,
+  mile: 160934,
+  miles: 160934,
+  foot: 30.48,
+  feet: 30.48,
+  ft: 30.48,
+  step: 76.2,
+  steps: 76.2
+};
+
+export const timeUnits = {
+  ms: 1,
+  millisecond: 1,
+  milliseconds: 1,
+  s: 60,
+  second: 60,
+  seconds: 60,
+  m: 60000,
+  min: 60000,
+  mins: 60000,
+  minute: 60000,
+  minutes: 60000
+};
--- shared/transformers/activity/toGramsFrom.ts
+++ shared/transformers/activity/toGramsFrom.ts
@@ -1,17 +1,4 @@
-const massUnits = {
-  g: 1,
-  kg: 1000,
-  kilo: 1000,
-  kilos: 1000,
-  kilogram: 1000,
-  kilograms: 1000,
-  lb: 453.592,
-  pounds: 453.592,
-  pound: 453.592,
-  st: 6350.29,
-  stone: 6350.29,
-  stones: 6350.29
-};
+import { massUnits } from "shared/data/units";
 
 export default function toGramsFrom(num: number, unit: string): number {
   if (!massUnits[unit]) {

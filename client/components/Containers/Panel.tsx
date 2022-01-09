c613ffd2793eb090c5120d02f929385f51a60cf8
Ryan Poe <baublet@gmail.com>
2022-01-09 16:38:40 -0600

    wip

--- client/components/Containers/Panel.tsx
+++ client/components/Containers/Panel.tsx
@@ -3,7 +3,10 @@ import cx from "classnames";
 
 export function Panel(props: React.PropsWithChildren<{ className?: string }>) {
   const classNames = cx(
-    "rounded-lg bg-slate-50 p-4 border border-slate-100 hover:bg-slate-100 hover:border-slate-200",
+    "hover:bg-slate-50 hover:bg-opacity-50",
+    "border border-slate-100 hover:border-slate-300",
+    "rounded-lg p-4",
+    "hover:shadow-slate-500/10 hover:shadow-lg",
     props.className
   );
   return <div className={classNames}>{props.children}</div>;

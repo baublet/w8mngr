a6e1f1614d0f82250f3405638334ef7a91731147
Ryan Poe <baublet@gmail.com>
2022-01-16 11:20:15 -0600

    Production deploy update

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   root: path.resolve(process.cwd(), "client"),
+  build: {
+    outDir: path.resolve(process.cwd(), "client-build"),
+  },
   define: {
     "process.env.GRAPHQL_ENDPOINT":
       "'//localhost:8080/.netlify/functions/graphql'",

98bf413b67e1881e055b4e49d4898558394691c2
Ryan Poe <baublet@gmail.com>
2022-01-20 09:28:58 -0600

    Try this

--- scripts/downloadDBCert.sh
+++ scripts/downloadDBCert.sh
@@ -6,4 +6,11 @@ else
   exit 1
 fi
 
+if [ -z ${DB_CERTIFICATE_DOWNLOAD_URL+x} ];
+  then
+    echo "DB_CERTIFICATE_DOWNLOAD_URL exists in env!";
+  else
+    export $(cat .env | xargs)
+  fi
+
 curl --create-dirs -o $HOME/.postgresql/root.crt -O "$DB_CERTIFICATE_DOWNLOAD_URL"

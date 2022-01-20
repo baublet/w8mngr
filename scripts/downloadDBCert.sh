c8882f441b86af0ed52b28a3ac8ce64c71e5a751
Ryan Poe <baublet@gmail.com>
2022-01-20 09:30:23 -0600

    Try this

--- scripts/downloadDBCert.sh
+++ scripts/downloadDBCert.sh
@@ -1,16 +1,7 @@
-if [ ! -f ../.env ]
-then
-  export $(cat .env | xargs)
+if [ -z ${DB_CERTIFICATE_DOWNLOAD_URL+x} ]; then
+  echo "DB_CERTIFICATE_DOWNLOAD_URL exists in env!"
 else
-  echo "No .env file found!"
-  exit 1
+  export $(cat .env | xargs)
 fi
-
-if [ -z ${DB_CERTIFICATE_DOWNLOAD_URL+x} ];
-  then
-    echo "DB_CERTIFICATE_DOWNLOAD_URL exists in env!";
-  else
-    export $(cat .env | xargs)
-  fi
 
 curl --create-dirs -o $HOME/.postgresql/root.crt -O "$DB_CERTIFICATE_DOWNLOAD_URL"

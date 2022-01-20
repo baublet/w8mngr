14ec05ceb4688864b68755ba3780d06e053804db
Ryan Poe <baublet@gmail.com>
2022-01-20 09:44:39 -0600

    Try this

--- scripts/downloadDBCert.sh
+++ scripts/downloadDBCert.sh
@@ -4,6 +4,6 @@ else
   export $(cat .env | xargs)
 fi
 
-echo "Downloading PG cert: $DB_CERTIFICATE_DOWNLOAD_URL"
+echo "Downloading PG cert: [$DB_CERTIFICATE_DOWNLOAD_URL]"
 
 curl --create-dirs -o $HOME/.postgresql/root.crt -O "$DB_CERTIFICATE_DOWNLOAD_URL"

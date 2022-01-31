f003780aa955b96026b8e73a67ec612ff47d4eff
Ryan Poe <baublet@gmail.com>
2022-01-31 12:25:52 -0600

    icons

--- netlify.sh
+++ netlify.sh
@@ -21,5 +21,12 @@ yarn build:client
 
 mkdir -p functions/config
 
+cp client/*.svg client-build/
+cp client/*.png client-build/
+cp client/*.ico client-build/
+cp client/*.xml client-build/
+cp client/*.svg client-build/
+cp client/*.webmanifest client-build/
+
 cp api/graphql.js functions/graphql.js
 cp api/config/schema.graphql functions/config/schema.graphql

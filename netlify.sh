7a6e4e7e3ec08102ba58acd60d47e845f56fe75b
Ryan Poe <baublet@gmail.com>
2022-01-31 18:29:02 -0600

    Permanent build

--- netlify.sh
+++ netlify.sh
@@ -3,17 +3,9 @@ set -e
 
 yarn gql:generate
 
-if [ "$BRANCH" == "master" ]; then
-  bash -c '$DOWNLOAD_DB_SSL_COMMAND'
+bash -c '$DOWNLOAD_DB_SSL_COMMAND'
 
-  # When we're ready to go live, use this
-  # yarn migrate:production
-
-  # In development, reset the database every deploy
-  yarn db:reset
-else
-  yarn db:reset:develop
-fi
+yarn db:migrate
 
 yarn build:css
 yarn build:api

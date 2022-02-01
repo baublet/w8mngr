e4d271bca1731ce036c70a392e8fb271a1c9001a
Ryan Poe <baublet@gmail.com>
2022-01-31 18:28:17 -0600

    UX

--- client/components/Auth/IsLoggedOut.tsx
+++ client/components/Auth/IsLoggedOut.tsx
@@ -3,8 +3,8 @@ import React from "react";
 import { useGetCurrentUserQuery } from "../../generated";
 
 export function IsLoggedOut({ children }: React.ComponentProps<any>) {
-  const { data } = useGetCurrentUserQuery({ fetchPolicy: "cache-first" });
-  if (data?.currentUser) {
+  const { data, loading } = useGetCurrentUserQuery({ fetchPolicy: "cache-first" });
+  if (loading || data?.currentUser) {
     return null;
   }
   return children;

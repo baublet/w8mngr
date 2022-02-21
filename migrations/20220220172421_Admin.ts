bab218f8f13b8b195bd6d89ef60626da89a1825b
Ryan Poe <baublet@gmail.com>
2022-02-20 21:25:19 -0600

    wip

---
+++ migrations/20220220172421_Admin.ts
@@ -0,0 +1,24 @@
+import { Knex } from "knex";
+
+import { attachSchema } from "../api/config/db";
+
+export async function up(knex: Knex): Promise<void> {
+  const results: { id: string }[] = await attachSchema(knex)
+    .select("*")
+    .from("user_account")
+    .where("sourceIdentifier", "=", "baublet@gmail.com");
+
+  if (results.length !== 1) {
+    throw new Error("Nope");
+  }
+
+  const result = results[0];
+
+  await attachSchema(knex)
+    .table("user")
+    .update({ role: "admin" })
+    .where("id", "=", result.id)
+    .limit(1);
+}
+
+export async function down(knex: Knex): Promise<void> {}
